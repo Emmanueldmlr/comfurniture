69faaa15045fa0503077cb64b261c5a4591d0fbe
Emmanueldmlr <bakaredmlr@gmail.com>
2021-03-30 16:04:39 +0100

    Push to prod

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ cssModules: true
 })
 module.exports = {
     env: {
-      baseUrl: 'http://dev-softassessment.herokuapp.com/',
+      baseUrl: 'http://prod-softassessment.herokuapp.com/',
       tokenName: 'Ecom-nRpfanqsQC',
       refreshTokenName: 'Ecom-hlxyDuKTge',
       cartId: 'Cart-xCvQz5I9Ox'
